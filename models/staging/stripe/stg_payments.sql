3a9a048a05ae3cd11ae792d42c1f0296faf515ab
Romulo Vieira <romulo.vieira777@gmail.com>
2022-09-16 12:47:16 -0300

    update stg_payments

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,9 +1,11 @@
 SELECT
-    "ID"            AS payment_id
-  , "ORDERID"       AS order_id
-  , "PAYMENTMETHOD" AS payment_method
-  , "STATUS"        AS status
-  , "AMOUNT" / 100  AS amount
-  , "CREATED"       AS created_at
+    "ID"                              AS payment_id
+  , "ORDERID"                         AS order_id
+  , "PAYMENTMETHOD"                   AS payment_method
+  , "STATUS"                          AS status
+  -- Amount is stored in cents, convert it to dollars
+  , "AMOUNT" / 100                    AS amount
+  , {{ cents_to_dollars('amount') }}  AS amount
+  , "CREATED"                         AS created_at
 FROM
    {{ source('stripe', 'payments') }}
