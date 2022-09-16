ef8714b22ff39cc3c0c6a6888c90b85701ed5872
Romulo Vieira <romulo.vieira777@gmail.com>
2022-09-16 12:05:17 -0300

    add int_orders_pivoted

---
+++ models/marts/core/int_orders_pivoted.sql
@@ -0,0 +1,31 @@
+{%- set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
+
+with payments AS (
+    SELECT
+        *
+    FROM
+        {{ ref('stg_payments') }}
+),
+pivoted AS (
+    SELECT
+        order_id,
+        {% for payment_method in payment_methods -%}
+
+        SUM(CASE WHEN payment_method = '{{ payment_method }}' THEN amount ELSE 0 END) AS {{ payment_method }}_amount
+
+        {%- if not loop.last -%}
+            ,
+        {%- endif %}
+
+        {% endfor -%}
+    FROM
+        payments
+    WHERE
+        status = 'success'
+    GROUP BY
+        1
+)
+SELECT
+    *
+FROM
+    pivoted
